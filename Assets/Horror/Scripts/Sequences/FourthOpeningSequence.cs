c3e207d5373c7f1653d4214c59938aec7e3bf0a8
Daniele Cortesi <dani.c93@hotmail.it>
2021-06-20 21:29:46 +0200

    Finish part 4 sequence with credits

--- Assets/Horror/Scripts/Sequences/FourthOpeningSequence.cs
+++ Assets/Horror/Scripts/Sequences/FourthOpeningSequence.cs
@@ -1,9 +1,12 @@
+using DG.Tweening;
+using Horror.Interaction;
 using Horror.UI;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 using Zenject;
 
 namespace Horror.Sequences
@@ -12,12 +15,24 @@ namespace Horror.Sequences
     {
         #region Inspector
 
+        [SerializeField]
+        private Transform monster = null;
 
+        [SerializeField]
+        private List<LightTargetBehaviour> lights = null;
 
         #endregion
 
+        [Inject(Id = "player.camera")]
+        private Transform playerCameraTransform = null;
+
+        [Inject(Id = "music")]
+        private AudioSource musicSource = null;
+
         [Inject]
         private ToastText toastText = null;
+
+        private bool lastSequenceStarted = false;
 
         private void Start()
         {
@@ -34,6 +49,45 @@ namespace Horror.Sequences
         {
             GetComponent<WakeUpSequence>().onSequenceEnd.RemoveListener(OnWakeUp);
             toastText.Show("Oh no... is there something behind me?");
+        }
+
+        private void Update()
+        {
+            if (lastSequenceStarted)
+                return;
+
+            Vector3 directionToMonster = (monster.position - playerCameraTransform.position).normalized;
+            float dot = Vector3.Dot(directionToMonster, playerCameraTransform.forward);
+
+            if (dot >= 0.6)
+            {
+                lastSequenceStarted = true;
+                StartCoroutine(LastSequenceCoroutine());
+            }
+        }
+
+        private IEnumerator LastSequenceCoroutine()
+        {
+            monster.GetComponent<ForceCameraLook>().enabled = true;
+
+            yield return new WaitForSeconds(2);
+
+            foreach (var light in lights)
+            {
+                light.Light.GetComponent<FlickeringLight>().StopFlicker();
+                light.Light.intensity = 0;
+            }
+
+            musicSource.DOFade(0, duration: 0.5f);
+
+            yield return new WaitForSeconds(2);
+
+            monster.GetComponent<AudioSource>().Play();
+
+            yield return new WaitForSeconds(6);
+
+            Cursor.lockState = CursorLockMode.None;
+            SceneManager.LoadSceneAsync("Credits", LoadSceneMode.Single);
         }
     }
     
