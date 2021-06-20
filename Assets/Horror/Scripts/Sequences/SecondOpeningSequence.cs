07fbc9a1214f3a262a75e597d5bd2d848e0c87d5
Daniele Cortesi <dani.c93@hotmail.it>
2021-06-20 15:58:11 +0200

    Add second part scene

---
+++ Assets/Horror/Scripts/Sequences/SecondOpeningSequence.cs
@@ -0,0 +1,42 @@
+using Horror.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Zenject;
+
+namespace Horror.Sequences
+{
+    public class SecondOpeningSequence : MonoBehaviour
+    {
+        #region Inspector
+
+
+
+        #endregion
+
+        [Inject]
+        private ToastText toastText = null;
+
+        private void Start()
+        {
+            GetComponent<WakeUpSequence>().onSequenceEnd.AddListener(OnWakeUp);
+            Invoke(nameof(ShowPhewMessage), 1f);
+        }
+
+        private void ShowPhewMessage()
+        {
+            toastText.Show("It was just a nightmare");
+        }
+
+        private void OnWakeUp()
+        {
+            GetComponent<WakeUpSequence>().onSequenceEnd.RemoveListener(OnWakeUp);
+            toastText.Show("Oh no...");
+        }
+    }
+    
+    [Serializable]
+    public class UnityEventSecondOpeningSequence : UnityEvent<SecondOpeningSequence> { }
+}
