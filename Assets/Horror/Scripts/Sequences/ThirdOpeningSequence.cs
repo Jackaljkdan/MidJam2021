8aca919d85522b1d4326a5eae0e0427d30b5f544
Daniele Cortesi <dani.c93@hotmail.it>
2021-06-20 19:01:22 +0200

    Add part 3 scene

--- Assets/Horror/Scripts/Sequences/ThirdOpeningSequence.cs
+++ Assets/Horror/Scripts/Sequences/ThirdOpeningSequence.cs
@@ -9,7 +9,7 @@ using Zenject;
 namespace Horror.Sequences
 {
     [RequireComponent(typeof(WakeUpSequence))]
-    public class SecondOpeningSequence : MonoBehaviour
+    public class ThirdOpeningSequence : MonoBehaviour
     {
         #region Inspector
 
@@ -28,16 +28,16 @@ namespace Horror.Sequences
 
         private void ShowPhewMessage()
         {
-            toastText.Show("I'm awake... It was just a nightmare");
+            toastText.Show("I'm awake now... right?!");
         }
 
         private void OnWakeUp()
         {
             GetComponent<WakeUpSequence>().onSequenceEnd.RemoveListener(OnWakeUp);
-            toastText.Show("Oh no...");
+            toastText.Show("No! Make it stop!");
         }
     }
     
     [Serializable]
-    public class UnityEventSecondOpeningSequence : UnityEvent<SecondOpeningSequence> { }
+    public class UnityEventThirdOpeningSequence : UnityEvent<ThirdOpeningSequence> { }
 }
